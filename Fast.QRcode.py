ba1f0c150d3b73e1eef3ee86ab01fe84b2680271
saitanaa <hck.botix@gmail.com>
2026-06-05 01:04:07 +0200

    Add files via upload

---
+++ Fast.QRcode.py
@@ -0,0 +1,24 @@
+import qrcode
+from tkinter import Tk, filedialog
+from urllib.parse import urlparse
+from pathlib import Path
+import os
+
+os.system("title QR4CIA")
+
+url = input("Enter .cia release URL: ")
+
+cia_name = Path(urlparse(url).path).name
+base_name = Path(cia_name).stem
+
+Tk().withdraw()
+output = filedialog.asksaveasfilename(
+    defaultextension=".png",
+    filetypes=[("PNG files", "*.png")],
+    initialfile=f"{base_name}-QR_CODE.png"
+)
+
+if output:
+    img = qrcode.make(url)
+    img.save(output)
+    print(f"Saved to {output}")
